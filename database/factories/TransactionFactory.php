9cb9e74e39de15b4a4ac0cc2574a096e2bf42dfc
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2025-12-01 17:25:12 -0300

    feat: add migration for personal access tokens table, user seeder, and initial API routes

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -17,7 +17,9 @@ class TransactionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "user_id"=> '1',
+            'amount'=> $this->faker->randomFloat(2, 1, 10000),
+            'type'=> $this->faker->randomElement(['income', 'expense']),
         ];
     }
 }
